e9183c27cdad0a6981b0e30c9f3b2400a678704d
Your Name <you@example.com>
2023-12-26 17:24:25 +0500

    => in process: each user should only see his expense no others

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,10 @@
 class ExpensesController < ApplicationController
   # before_action :authenticate_user!
   before_action :authenticate_user!, only: [:create, :update, :destroy]
+  # before_action :set_expense, only: [:show, :edit, :update, :destroy]
     def index
-        @expense = Expense.all
+         @expense = Expense.all
+        # @expense = current_user.expenses
     end
 
     def new
@@ -11,8 +13,8 @@ class ExpensesController < ApplicationController
 
 
     def create
-        @expense = Expense.new(post_params)
-        
+         @expense = Expense.new(expense_params)
+        # @expense = current_user.expenses.new(expense_params)
         if(@expense.save)
           flash[:notice]="Expense created successfully" 
          redirect_to @expense
@@ -22,18 +24,18 @@ class ExpensesController < ApplicationController
         end
 
     def show
-         @expense = Expense.find(params[:id])
+          @expense = Expense.find(params[:id])
 
     end
 
     def edit
-        @expense = Expense.find(params[:id])
+          @expense = Expense.find(params[:id])
 
    end
 
    def update
-    @expense=Expense.find(params[:id])
-    if(@expense.update(post_params))
+      @expense=Expense.find(params[:id])
+    if(@expense.update(expense_params))
       flash[:notice]="Expense Updated successfully"
     redirect_to @expense
     else
@@ -50,8 +52,18 @@ class ExpensesController < ApplicationController
           end
 
 
-    private def post_params
-        params.require(:expense).permit(:name , :category_id, :amount, :date_time, :start_time)
-      end
+    private
+    
+    def expense_params
+        params.require(:expense).permit(:name , :category_id, :amount, :date_time, :start_time, :user_id)
+    end
+      
+        # def set_expense
+        #   @expense = current_user.expenses.find(params[:id])
+        # end
+      
+      
 
 end
+
+
--- db/migrate/20231224122746_create_expenses.rb
+++ db/migrate/20231224122746_create_expenses.rb
@@ -4,7 +4,7 @@ class CreateExpenses < ActiveRecord::Migration[5.2]
       t.string :name
       t.integer :amount
       t.datetime :start_time
-    
+      t.belongs_to :user
       t.timestamps
     end
   end
